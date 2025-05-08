ba295e06952640a245ee8af3ed4f8d77b10a192d
tom/spooks <zimalwayswins@gmail.com>
2025-05-07 20:50:04 -0400

    Create layout.tsx

---
+++ layout.tsx
@@ -0,0 +1,27 @@
+import type React from "react"
+import "@/app/globals.css"
+import { Inter } from "next/font/google"
+import { ThemeProvider } from "@/components/theme-provider"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata = {
+  title: "Vanity - The most advanced Roblox Executor",
+  description: "Elevate your Roblox experience with Vanity, the most powerful and reliable Roblox executor available.",
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
